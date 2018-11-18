b49fb827a854b14d4c7802d6175d27cbbd98176a
ayns <peenats1203@gmail.com>
2018-11-18 15:39:43 +0900

    Modified copping view

--- app/src/main/java/com/example/ayana/chekikkov1/EditActivity.java
+++ app/src/main/java/com/example/ayana/chekikkov1/EditActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.graphics.Rect;
