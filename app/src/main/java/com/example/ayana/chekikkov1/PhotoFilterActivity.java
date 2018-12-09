41b137f2498f83306ae64dfbb283e8a942de6d07
ayns <peenats1203@gmail.com>
2018-12-09 18:37:23 +0900

    Merge 2 images

--- app/src/main/java/com/example/ayana/chekikkov1/PhotoFilterActivity.java
+++ app/src/main/java/com/example/ayana/chekikkov1/PhotoFilterActivity.java
@@ -2,6 +2,7 @@ package com.example.ayana.chekikkov1;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,8 @@ import java.io.ByteArrayOutputStream;
 
 public class PhotoFilterActivity extends AppCompatActivity {
     Bitmap bmp;
+    Bitmap frameImage;
+    Bitmap mergedImages;
     ImageView mfinalImageView;
 
     @Override
@@ -23,7 +26,21 @@ public class PhotoFilterActivity extends AppCompatActivity {
         byte[] byteArray = extras.getByteArray(CropActivity.EXTRA_CROPPED_IMAGE);
         bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         mfinalImageView = findViewById(R.id.finalImageView);
-        mfinalImageView.setImageBitmap(bmp);
+//        mfinalImageView.setImageBitmap(bmp);
 
+        frameImage = BitmapFactory.decodeResource(getResources(), R.drawable.frame_2x);
+        mergedImages = createSingleImageFromMultipleImages(frameImage, bmp);
+
+        mfinalImageView.setImageBitmap(mergedImages);
+
+    }
+
+    private Bitmap createSingleImageFromMultipleImages(Bitmap firstImage, Bitmap secondImage){
+
+        Bitmap result = Bitmap.createBitmap(firstImage.getWidth(), firstImage.getHeight(), firstImage.getConfig());
+        Canvas canvas = new Canvas(result);
+        canvas.drawBitmap(firstImage, 0f, 0f, null);
+        canvas.drawBitmap(secondImage, 25, 25, null);
+        return result;
     }
 }
